ff75ae9c18bd4dc32b3c83d73327db1f1e597e99
mindaugl <mindelek@gmail.com>
2025-06-05 15:28:21 +0300

    Add, update index, detail and give_review views.

--- review/books/urls.py
+++ review/books/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "books"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:book_id>/", views.detail, name="detail"),
+    path("<int:book_id>/give_review/", views.give_review, name="give_review"),
 ]
--- review/books/views.py
+++ review/books/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the books index.")
+    books_top10 = Book.objects.order_by("-rating")[:10]
+    context = {"books_top10": books_top10}
+    return render(request, "books/index.html", context)
+
+def detail(request, book_id):
+    # return HttpResponse("You're looking at book %s." % book_id)
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, "books/detail.html", {"book": book})
+
+def give_review(request, book_id):
+    return HttpResponse("You're reviewing book %s." % book_id)
