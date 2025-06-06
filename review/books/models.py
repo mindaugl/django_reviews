44c8363f70a7f73fc5c05e262a2668615d962d6d
mindaugl <mindelek@gmail.com>
2025-06-06 18:50:27 +0300

    Add voting page.

--- review/books/models.py
+++ review/books/models.py
@@ -5,7 +5,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
+    reviews = models.IntegerField(default=100)
 
     def __str__(self):
         return f"'{self.title}' by '{self.author}', published on the '{self.publication_date}'"
