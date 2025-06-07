91c9dd20a202d60f3a66509dae115f849318a77d
mindaugl <mindelek@gmail.com>
2025-06-07 14:59:02 +0300

    Remove comments.

--- review/books/urls.py
+++ review/books/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("all/", views.all_books, name="all"),
     path("<int:book_id>/", views.detail, name="detail"),
     path("<int:book_id>/give_review/", views.give_review, name="give_review"),
-    # path("<int:book_id>/<int:rating>/give_review/", views.give_review, name="give_review"),
 ]
--- review/books/views.py
+++ review/books/views.py
@@ -11,7 +11,6 @@ def index(request):
     return render(request, "books/index.html", context)
 
 def detail(request, book_id):
-    # return HttpResponse("You're looking at book %s." % book_id)
     book = get_object_or_404(Book, pk=book_id)
     return render(request, "books/detail.html", {"book": book})
 
